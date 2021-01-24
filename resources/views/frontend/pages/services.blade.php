e269d633d2991f1b1e9354e5c0f9d8e22357f534
freejob985 <freejob985@gmail.com>
2021-01-24 13:32:42 +0200

    asd

--- resources/views/frontend/pages/services.blade.php
+++ resources/views/frontend/pages/services.blade.php
@@ -5,6 +5,7 @@
 @endsection
 @section('site-title')
     {{get_static_option('service_page_'.get_user_lang().'_name')}} {{__('Category:')}} {{$category_name}}
+    <p>{{ $a1 }}</p>
 @endsection
 @section('content')
     <section class="blog-content-area padding-100">
