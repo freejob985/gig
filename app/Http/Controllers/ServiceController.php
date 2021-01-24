7d1b0dd6f2e98b11c25b2cc26b769b9fab0e2242
freejob985 <freejob985@gmail.com>
2021-01-24 11:23:18 +0200

    asd

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\ServiceCategory;
 use App\Services;
 use Illuminate\Http\Request;
-use Intervention\Image\Facades\Image;
 
 class ServiceController extends Controller
 {
@@ -15,60 +14,104 @@ class ServiceController extends Controller
         $this->middleware('auth:admin');
     }
 
-    public function index(){
+    public function index()
+    {
         $all_services = Services::all()->groupBy('lang');
-        $service_category = ServiceCategory::where(['status' => 'publish','lang' => 'en'])->get();
-        return view('backend.pages.service.index')->with(['all_services' => $all_services,'service_category' => $service_category]);
+        $service_category = ServiceCategory::where(['status' => 'publish', 'lang' => 'en'])->get();
+        return view('backend.pages.service.index')->with(['all_services' => $all_services, 'service_category' => $service_category]);
     }
 
-    public function store(Request $request){
-        $this->validate($request,[
+    public function store(Request $request)
+    {
+        $this->validate($request, [
             'title' => 'required|string|max:191',
             'icon' => 'required|string|max:191',
             'lang' => 'required|string|max:191',
             'description' => 'required|string',
             'excerpt' => 'required|string',
             'categories_id' => 'required|string',
-            'image' => 'nullable|string|max:191'
+            'image' => 'nullable|string|max:191',
         ]);
-        Services::create($request->all());
 
-        return redirect()->back()->with(['msg' => 'New service Added...','type' => 'success']);
+//         array:6 [▼
+//   "_token" => "h8rOnA9efEro1GiFQgxt6KAQZ5irJSVaSODu4fDk"
+//   "lang" => "fr"
+//   "name" => "a"
+//   "a1" => "a"
+//   "status" => "draft"
+//   "a2" => UploadedFile {#858 ▶}
+// ]
+
+
+    //     array:6 [▼
+    //     "_token" => "h8rOnA9efEro1GiFQgxt6KAQZ5irJSVaSODu4fDk"
+    //     "lang" => "fr"
+    //     "name" => "a"
+    //     "a1" => "a"
+    //     "status" => "draft"
+    //     "a2" => UploadedFile {#858 ▶}
+    //   ]
+
+
+    
+    if ($request->hasFile('a2')) {
+        $file = $request->a2;
+        $extension = $file->getClientOriginalExtension();
+        $filename = rand(111, 99999) . "_mrbean" . '.' . $extension;
+       // $file->move("assets/front/img/Logo/", $filename);
+        $file->move(public_path() . '/files/', $filename);
+
+    } else {
+
+        $filename = "";
+
+    }
+        $array = array();
+        $array['lang'] = $request->input('lang');
+        $array['name'] = $request->input('name');
+        $array['a1'] = $request->input('a1');
+        $array['status'] = $request->input('status');
+        $array['a2'] = $filename;
+        Services::create($request->all());
+        return redirect()->back()->with(['msg' => 'New service Added...', 'type' => 'success']);
     }
 
-    public function update(Request $request){
+    public function update(Request $request)
+    {
 
-        $this->validate($request,[
+        $this->validate($request, [
             'title' => 'required|string|max:191',
             'lang' => 'required|string|max:191',
             'icon' => 'required|string|max:191',
             'description' => 'required|string',
             'excerpt' => 'required|string',
             'categories_id' => 'required|string',
-            'image' => 'nullable|string|max:191'
+            'image' => 'nullable|string|max:191',
         ]);
         Services::find($request->id)->update($request->all());
 
-        return redirect()->back()->with(['msg' => 'Service Item Updated...','type' => 'success']);
+        return redirect()->back()->with(['msg' => 'Service Item Updated...', 'type' => 'success']);
     }
 
-    public function delete($id){
+    public function delete($id)
+    {
         Services::find($id)->delete();
 
-        return redirect()->back()->with(['msg' => 'Delete Success...','type' => 'danger']);
+        return redirect()->back()->with(['msg' => 'Delete Success...', 'type' => 'danger']);
     }
 
-    public function category_index(){
+    public function category_index()
+    {
         $all_category = ServiceCategory::all()->groupBy('lang');
         return view('backend.pages.service.category')->with(['all_category' => $all_category]);
     }
-    public function category_store(Request $request){
-        $this->validate($request,[
+    public function category_store(Request $request)
+    {
+        $this->validate($request, [
             'name' => 'required|string|max:191',
             'lang' => 'required|string|max:191',
-            'status' => 'required|string|max:191'
+            'status' => 'required|string|max:191',
         ]);
-
 
         dd($request->all());
 
@@ -76,20 +119,21 @@ class ServiceController extends Controller
 
         return redirect()->back()->with([
             'msg' => 'New Category Added...',
-            'type' => 'success'
+            'type' => 'success',
         ]);
     }
 
-    public function category_update(Request $request){
-        $this->validate($request,[
+    public function category_update(Request $request)
+    {
+        $this->validate($request, [
             'name' => 'required|string|max:191',
             'lang' => 'required|string|max:191',
-            'status' => 'required|string|max:191'
+            'status' => 'required|string|max:191',
         ]);
 
         ServiceCategory::find($request->id)->update([
             'name' => $request->name,
-            'lang' =>  $request->lang,
+            'lang' => $request->lang,
             'status' => $request->status,
             'a1' => $request->status,
             'a2' => $request->status,
@@ -97,26 +141,28 @@ class ServiceController extends Controller
 
         return redirect()->back()->with([
             'msg' => 'Category Update Success...',
-            'type' => 'success'
+            'type' => 'success',
         ]);
     }
 
-    public function category_delete(Request $request,$id){
-        if (Services::where('categories_id',$id)->first()){
+    public function category_delete(Request $request, $id)
+    {
+        if (Services::where('categories_id', $id)->first()) {
             return redirect()->back()->with([
                 'msg' => 'You Can Not Delete This Category, It Already Associated With A Service...',
-                'type' => 'danger'
+                'type' => 'danger',
             ]);
         }
         ServiceCategory::find($id)->delete();
         return redirect()->back()->with([
             'msg' => 'Category Delete Success...',
-            'type' => 'danger'
+            'type' => 'danger',
         ]);
     }
 
-    public function category_by_slug(Request $request){
-        $service_category = ServiceCategory::where(['status' => 'publish','lang' => $request->lang])->get();
+    public function category_by_slug(Request $request)
+    {
+        $service_category = ServiceCategory::where(['status' => 'publish', 'lang' => $request->lang])->get();
         return response()->json($service_category);
     }
 }
